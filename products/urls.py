c759406d1e36b4b6d72e334e46189a1bf7fafc94
mathiase4 <mathiaseriksson98@hotmail.com>
2025-10-24 19:24:13 +0200

    created a home.html page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # URL patterns for the products app
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Product
 
+
+def home(request):
+    """
+
+Display homepage with hero section and all products.
+    """
+    products = Product.objects.all()
+    return render(request, 'products/home.html', {'products': products})
+
+
 def product_list(request):
+    """
+    View to show all products,
+    Shows all mugs available in the shop.
+    """
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
 
