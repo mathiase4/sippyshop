af94398d9ad5b1c3dc617ebec4c698513dcd7039
mathiase4 <mathiaseriksson98@hotmail.com>
2025-11-02 21:20:05 +0100

    added price ar the time of order and model for item in an order

--- products/models.py
+++ products/models.py
@@ -42,4 +42,31 @@ class Order(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     
-    #
+    #stripe payment 
+    stripe_payment_intent = models.CharField(max_length=200, blank=True)
+    
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at'] # newest order 
+    
+    def __str__(self):
+        return f"Order {self.id} - {self.full_name}"
+    
+class OrderItem(models.Model):
+    """
+    Model for items in an order.
+    """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product =   models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2) # price at the time of order
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
+    def get_total_price(self):
+        """ calculate total price """
+        return self.quantity * self.price
