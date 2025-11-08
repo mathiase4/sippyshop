971a4d5e0aa2edf158292e364753d43ab2dd6f79
mathiase4 <mathiaseriksson98@hotmail.com>
2025-11-08 19:00:48 +0100

    added order history icon to navbar for easy access when you logged in.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # main site for order history
-    path('orders/', views.order_history, name='order_history'),
+    path('history/', views.order_history, name='order_history'),
     
     # detail site for orders
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
--- sippyshop_project/urls.py
+++ sippyshop_project/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
     
         # password send message
     path('password/reset/',
