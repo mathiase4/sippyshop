cf456ce4ca1132b250ebe816de5cddb893cdbd53
mathiase4 <mathiaseriksson98@hotmail.com>
2025-11-09 22:17:44 +0100

    correct in allowed hosts for heroku

--- sippyshop_project/settings.py
+++ sippyshop_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)  # converts true string to boolean
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sippyshop-project.herokuapp.com"]
 
 
 # Application definition
